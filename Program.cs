36caba3e05472d83dacb18f14ae42b4a04e37212
Matilda578 <101889190@student.swin.edu.au>
2020-04-09 16:52:08 +1000

    well it doesnt work but here

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstlist = new List<int>(); 
+// user enters 5 numbers store as ints within a list
+           { 
+              
+            var userentered = Console.ReadLine("");}
+            {
+
+                var userentered = Int32.Parse(listnumber);
+                int listnumber = 0;
+                if (listnumber >= 5)
+                    return;
+                Console.WriteLine("Please enter another number");
+            }
+        }
+                       
+    }
+}
